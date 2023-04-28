54336a81d319e7fd2b9162968ed083678272c9b6
sharifbekk <mustafozodashq888@gmail.com>
2023-04-28 07:09:40 +0500

    last commit

---
+++ index.js
@@ -0,0 +1,88 @@
+
+
+
+
+
+
+
+
+
+{/* <button onclick="alert('Hello world!')">Click me</button> */ }
+
+
+
+
+
+
+// // get the div element
+// const divElement = document.getElementById("myDiv");
+
+// // set the innerHTML of the div element
+// divElement.innerHTML = "<h1>Hello World!</h1><p>This is some text.</p>";
+
+
+
+
+
+// // get the div element
+// const divElement = document.getElementById("myDiv");
+
+// // get the current innerHTML of the div element
+// const htmlContent = divElement.innerHTML;
+
+
+
+
+
+// // create a new div element
+// const divElement = document.createElement("div");
+
+// // add some text content to the div element
+// divElement.textContent = "This is a new div element!";
+
+// // append the div element to the body of the page
+// document.body.appendChild(divElement);
+
+
+
+
+
+// // get the parent element
+// const parent = document.getElementById("parentElement");
+
+// // create a new element
+// const newElement = document.createElement("div");
+// newElement.textContent = "This is a new element!";
+
+// // append the new element to the parent element
+// parent.appendChild(newElement);
+
+
+
+
+// const element = document.getElementById("myElement");
+// element.classList.add("newClass");
+
+
+
+
+
+
+
+// const element = document.getElementById("myElement");
+// element.classList.remove("oldClass");
+
+
+
+
+
+
+const element = document.getElementById("myElement");
+element.classList.toggle("active");
+
+
+
+
+
+
+
